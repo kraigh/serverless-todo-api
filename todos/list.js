87f62a48bd2ca45668e142f6e906d606a09716e9
Kraig Hufstedler <kraigory@gmail.com>
2019-04-10 22:01:44 -0500

    nodejs8

--- todos/list.js
+++ todos/list.js
@@ -9,17 +9,6 @@ if (!AWS.config.region) {
 }
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-new AWS.DynamoDB({
-  httpOptions: {
-    agent: new https.Agent({
-      rejectUnauthorized: true,
-      secureProtocol: "TLSv1_method",
-      ciphers: "ALL"
-    })
-  }
-}).DocumentClient();
-
-
 module.exports.list = (event, context, callback) => {
     const headers = {
         "Access-Control-Allow-Origin": "*",
